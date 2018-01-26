7ee683d766284b5171764b4710e6b9f3388c5a8e
Evan Darst <evan.l.darst@gmail.com>
2018-01-26 08:57:54 -0500

    updating with a post route for creating new burgers

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -4,12 +4,36 @@ var Sequelize = require("sequelize");
 // var router = express.Router();
 var burger = require("../models/burger.js");
 
+//get app for grabbing our index of burgers
+
 app.get("/index", function(req, res) {
 
-  db.Burger.findALL
-})
+  db.Burger.findALL ({
 
-//post app
+    order: ["burger_name"]
+  }).then(function(data) {
+
+    //get all the burgers
+  });
+
+  //push back to handlebars
+
+  res.render(index);
+});
+
+//post app to create new burgers
+
+app.post("/", function(req, res) {
+
+  db.Burger.create (
+
+      burger_name: req.body.burger_name }
+
+  ).then(function() {
+
+    res.redirect
+  })
+}
 
 //put app
 
